cc05c247c6db0e55dd4381af9ccf21f1039b2bdd
Levente Kale <levente.kale@nokia.com>
2020-01-15 16:44:27 +0100

    Added SCM and K8s integration artifacts

--- scm/build/build.sh
+++ scm/build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 export GOOS=linux
 cd $GOPATH/src/github.com/nokia/danm-utils
-$GOPATH/bin/glide install --strip-vendor
+glide install --strip-vendor
 go get -d github.com/vishvananda/netlink
-go build github.com/nokia/danm-utils/cmd/cleaner
+go install github.com/nokia/danm-utils/cmd/cleaner
